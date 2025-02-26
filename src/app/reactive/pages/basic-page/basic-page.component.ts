84cd90bd708baa44b82d1f164f3b119e0e8e2d54
garbettbrandon <garbettbrandon@gmail.com>
2025-02-26 15:24:28 +0100

    terminando seccion 14

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,49 @@
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [JsonPipe, ReactiveFormsModule],
+  templateUrl: './basic-page.component.html',
+})
+export class BasicPageComponent {
+  private fb = inject(FormBuilder);
+
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(10)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
+  });
+
+  // Forma tradicional de hacer los Forms
+
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({
+      price: 0,
+      inStorage: 0,
+    });
+  }
+}
